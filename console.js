79cf98ccbff93131336dd7cd9a2665ef01789375
Shaykot Hossain Selim <86794875+shaykotselim@users.noreply.github.com>
2023-03-19 14:46:27 +0600

    Update console.js

--- console.js
+++ console.js
@@ -1,2 +1,34 @@
 // here is comment
-console.log("Hello-world);
+//console.log("Hello-world);
+  // function factorialNumber (numbers){
+//     let result = 1;
+//     for(let i = 1; i <= numbers ; i++){
+//         result = result * i;
+
+//     }
+//     return result;
+// }
+
+//  const totalSum = factorialNumber(7);
+//  console.log(totalSum);
+
+ 
+//  function factorial (numbers){
+//     let result = 1;
+//     for(let i = numbers; i >= 1; i--){
+//         result = result  * i;
+//     }
+//     return result;
+//  }
+// const totalFact = factorial(7);
+// console.log(totalFact)
+
+// function print(a, b, c){
+//     return a+2;
+//     return a*b;
+//     return b*c+a;
+//   }
+//   console.log(print(1,2,3));
+let ul = `<li>Programming<li>`; 
+ul +=  `<li>Hero<li>`; 
+console.log(ul);         
